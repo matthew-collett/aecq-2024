00d5d8524cbb7bbe0fc44f310aab15295de8b05e
matthew-collett <matthew.collett12@gmail.com>
2024-11-02 09:21:07 -0300

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from '@state/AuthProvider'
+// import { AuthProvider } from '@state/AuthProvider'
 import AppLayout from '@layouts/AppLayout'
 import ProtectedRoute from '@layouts/ProtectedRoute'
 import Dashboard from '@views/Dashboard'
@@ -7,17 +7,17 @@ import Dashboard from '@views/Dashboard'
 const App = () => {
   return (
     <Router>
-      <AuthProvider>
-        <Routes>
-          {/* <Route path="/login" element={<LoginView />} /> */}
-          {/* <Route path="/register" element={<RegisterView />} /> */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<AppLayout />}>
-              <Route index element={<Dashboard />} />
-            </Route>
-          </Route>
-        </Routes>
-      </AuthProvider>
+      {/* <AuthProvider> */}
+      <Routes>
+        {/* <Route path="/login" element={<LoginView />} /> */}
+        {/* <Route path="/register" element={<RegisterView />} /> */}
+        {/* <Route element={<ProtectedRoute />}> */}
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+        {/* </Route> */}
+      </Routes>
+      {/* </AuthProvider> */}
     </Router>
   )
 }
